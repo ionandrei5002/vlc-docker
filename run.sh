a8aee4f2d7a3a97879f051a37ba8657cbebc48c0
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2019-11-27 15:40:26 +0200

    +modif

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run --rm -it -e DISPLAY=$DISPLAY \
+nohup docker run --rm -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e PULSE_SERVER=$XDG_RUNTIME_DIR/pulse/native \
     -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse \
     -v /home/andrei/Music:/home/andrei/Music \
-    vlc-docker:latest
+    vlc-docker:latest &
